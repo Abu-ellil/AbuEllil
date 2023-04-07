f239382c3df831fe69a49fb028e48f92f187b393
Abo Ellil <mr.abuellil@gmail.com>
2023-04-07 05:02:01 +0200

    d

--- src/components/Certifications .js
+++ src/components/Certifications .js
@@ -9,7 +9,7 @@ const Certifications = () => {
       image:
         "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/code.png?raw=true",
       issuer: "Issuer 1",
-      date: "May 2021",
+      date: "December 2022",
       link: "https://www.udemy.com/certificate/UC-a731ae10-edd2-4291-906b-0b148d8703f0/",
     },
     {
@@ -17,7 +17,7 @@ const Certifications = () => {
       image:
         "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/js.png?raw=true",
       issuer: "Issuer 2",
-      date: "June 2021",
+      date: "February 2023",
       link: "https://www.udemy.com/certificate/UC-11cd8a7a-da8d-4ec4-8dd7-c9545f0edebb/",
     },
     {
@@ -25,7 +25,7 @@ const Certifications = () => {
       image:
         "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/reactjs.png?raw=true",
       issuer: "Issuer 2",
-      date: "June 2021",
+      date: "April 2023",
       link: "https://www.udemy.com/course/react-the-complete-guide-incl-redux",
     },
     {
@@ -33,15 +33,15 @@ const Certifications = () => {
       image:
         "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/node.png?raw=true",
       issuer: "Issuer 3",
-      date: "July 2021",
+      date: "May 2023",
       link: "https://www.udemy.com/course/nodejs-the-complete-guide/",
     },
     {
       name: "Certification 3",
       image:
         "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/1670172004596.png?raw=true",
-      issuer: "Issuer 3",
-      date: "July 2021",
+      issuer: "Issuer 4",
+      date: "December, 2022",
       link: "https://www.linkedin.com/learning/certificates/7e74ef7e58ab08aa3be9d7ab3cf3cb237a2d9527bb68b87b871905c225e6996b",
     },
   ];
