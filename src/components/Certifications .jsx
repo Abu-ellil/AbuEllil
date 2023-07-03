5d33c5a0d9aa1f533f1431615e6ac251c67aa8ad
Abo Ellil <mr.abuellil@gmail.com>
2023-07-03 17:32:59 +0300

    img url update

--- src/components/Certifications .jsx
+++ src/components/Certifications .jsx
@@ -3,42 +3,42 @@ const Certifications = () => {
     {
       name: "Certification 1",
       image:
-        "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/code.png?raw=true",
+        "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/code.png?raw=true",
       issuer: " Build Responsive Real-World Websites with HTML and CSS",
       date: "December 2022",
-      link: "https://www.udemy.com/certificate/UC-a731ae10-edd2-4291-906b-0b148d8703f0/",
+      link: "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/code.png?raw=true",
     },
     {
       name: "Certification 2",
       image:
-        "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/js.png?raw=true",
+        "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/js.png?raw=true",
       issuer: "JavaScript - The Complete Guide 2023 (Beginner + Advanced)",
       date: "February 2023",
-      link: "https://www.udemy.com/certificate/UC-11cd8a7a-da8d-4ec4-8dd7-c9545f0edebb/",
+      link: "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/js.png?raw=true",
     },
     {
       name: "Certification 2",
       image:
-        "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/reactjs.png?raw=true",
+        "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/reactjs.png?raw=true",
       issuer: "React - The Complete Guide (incl Hooks, React Router, Redux)",
       date: "April 2023",
-      link: "https://www.udemy.com/course/react-the-complete-guide-incl-redux",
+      link: "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/reactjs.png?raw=true",
     },
     {
       name: "Certification 3",
       image:
-        "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/node.png?raw=true",
+        "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/node.png?raw=true",
       issuer: "NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)",
       date: "May 2023",
-      link: "https://www.udemy.com/course/nodejs-the-complete-guide/",
+      link: "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/node.png?raw=true",
     },
     {
       name: "Certification 3",
       image:
-        "https://github.com/Abu-ellil/AbuEllil/blob/main/src/img/certf/1670172004596.png?raw=true",
+        "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/1670172004596.png?raw=true",
       issuer: "Programming Foundations: Beyond the Fundamentals",
       date: "December, 2022",
-      link: "https://www.linkedin.com/learning/certificates/7e74ef7e58ab08aa3be9d7ab3cf3cb237a2d9527bb68b87b871905c225e6996b",
+      link: "https://github.com/Abu-ellil/AbuEllil-portfolio/blob/main/src/assets/certf/1670172004596.png?raw=true",
     },
   ];
 
