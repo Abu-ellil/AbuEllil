fb7b545776f796864582a71c091d6b7f46724121
Abo Ellil <mr.abuellil@gmail.com>
2023-07-04 17:37:04 +0300

    11

--- src/components/ToTopButton.jsx
+++ src/components/ToTopButton.jsx
@@ -25,15 +25,58 @@ const ToTopButton = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+
+
+
+  const [circumference, setCircumference] = useState(0);
+ let size = 200;
+ let strokeWidth = 10;
+ let progress = scrollPercent;
+  useEffect(() => {
+    const radius = size / 2;
+    const circleCircumference = 2 * Math.PI * radius;
+    setCircumference(circleCircumference);
+  }, [size]);
+
+  const progressOffset = circumference - (progress / 100) * circumference;
+
+
+
   return (
     <div
       className="to-top-button"
       style={{ opacity: scrollPercent > 10 ? 1 : 0 }}
     >
-      <button onClick={scrollToTop}>
-        <span>To Top</span>
+     
+      <svg className="circular-progress" width={size} height={size}>
+        <circle
+          className="circular-progress-bg"
+          cx={size / 2}
+          cy={size / 2}
+          r={size / 2 - strokeWidth / 2}
+          strokeWidth={strokeWidth}
+        />
+
+
+ {/* <button
+        onClick={scrollToTop}
+        style={{ border: `${(scrollPercent / 100) * 10}px solid #00eeff` }}
+      >
+        <span>Top</span>
         <span className="scroll-percent">{scrollPercent}%</span>
-      </button>
+      </button> */}
+jhkhjkjhk
+
+        <circle
+          className="circular-progress-fill"
+          cx={size / 2}
+          cy={size / 2}
+          r={size / 2 - strokeWidth / 2}
+          strokeWidth={strokeWidth}
+          strokeDasharray={circumference}
+          strokeDashoffset={progressOffset}
+        />
+      </svg>
     </div>
   );
 };
